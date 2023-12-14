4d5b098ef05306b59887858ebae89e756e7575a7
Tigana Bryan <tiganabusinessinquiries@gmail.com>
2023-12-13 21:41:08 -0500

    remove unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,4 @@
-import { Inter } from "next/font/google";
-import type { AppProps } from "next/app";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home({ Component, pageProps }: AppProps) {
+export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			TEST
