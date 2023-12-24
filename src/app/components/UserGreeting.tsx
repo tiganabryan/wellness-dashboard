19ec5879386932ca8698806edbefec0d56eae75f
Tigana Bryan <tiganabusinessinquiries@gmail.com>
2023-12-23 19:05:27 -0500

    add background gradient to UserGreeting.tsx
    still need to change gradient angle

--- src/app/components/UserGreeting.tsx
+++ src/app/components/UserGreeting.tsx
@@ -2,7 +2,13 @@ import React from "react";
 
 const UserGreeting = () => {
 	const user = "tigana";
-	return <div className="">good morning, {user}</div>;
+	return (
+		<React.Fragment>
+			<h1 className="px-3 py-1 bg-gradient-to-r from-light-pink to-magenta text-white w-3/5 rounded-lg">
+				good morning, {user} 🤍
+			</h1>
+		</React.Fragment>
+	);
 };
 
 export default UserGreeting;
