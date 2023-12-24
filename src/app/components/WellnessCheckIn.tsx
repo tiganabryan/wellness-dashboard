2b09a7107fd6e50f9a47d221820ea5e8dc14cc12
Tigana Bryan <tiganabusinessinquiries@gmail.com>
2023-12-24 14:54:54 -0500

    add name to WellnessCheckIn object and use as htmlFor, name, and id of cards

--- src/app/components/WellnessCheckIn.tsx
+++ src/app/components/WellnessCheckIn.tsx
@@ -3,6 +3,7 @@ import WellnessCheckInCard from "./WellnessCheckInCard";
 
 const WellnessCheckIn = () => {
 	interface checkInCard {
+		name: string;
 		question: string;
 		option1text: string;
 		option2text: string;
@@ -10,21 +11,25 @@ const WellnessCheckIn = () => {
 
 	const checkInCards: checkInCard[] = [
 		{
+			name: "water",
 			question: "did you drink enough water today?",
 			option1text: "yes",
 			option2text: "no",
 		},
 		{
+			name: "stress",
 			question: "how stressed were you today?",
 			option1text: "just a little",
 			option2text: "very stressed",
 		},
 		{
+			name: "meals",
 			question: "did you eat 3 meals today?",
 			option1text: "yes",
 			option2text: "no",
 		},
 		{
+			name: "conversation",
 			question: "did you have an uplifting conversation today?",
 			option1text: "yes",
 			option2text: "no",
@@ -36,6 +41,7 @@ const WellnessCheckIn = () => {
 			{checkInCards.map((checkInCard, index) => (
 				<WellnessCheckInCard
 					key={index}
+					name={checkInCard.name}
 					question={checkInCard.question}
 					option1text={checkInCard.option1text}
 					option2text={checkInCard.option2text}
--- src/app/components/WellnessCheckInCard.tsx
+++ src/app/components/WellnessCheckInCard.tsx
@@ -5,11 +5,23 @@ const WellnessCheckInCard = (checkInCardData: any) => {
 		<React.Fragment>
 			<h4>{checkInCardData.question}</h4>
 
-			<label htmlFor="option1">{checkInCardData.option1text}</label>
-			<input type="radio" name="option1" id="option1" />
+			<label htmlFor={checkInCardData.name}>
+				{checkInCardData.option1text}
+			</label>
+			<input
+				type="radio"
+				name={checkInCardData.name}
+				id={checkInCardData.name}
+			/>
 
-			<label htmlFor="option1">{checkInCardData.option2text}</label>
-			<input type="radio" name="option2" id="option2" />
+			<label htmlFor={checkInCardData.name}>
+				{checkInCardData.option2text}
+			</label>
+			<input
+				type="radio"
+				name={checkInCardData.name}
+				id={checkInCardData.name}
+			/>
 		</React.Fragment>
 	);
 };
