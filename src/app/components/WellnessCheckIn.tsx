d0d95458f93016d9c790ed556f9d879296f801fa
Tigana Bryan <tiganabusinessinquiries@gmail.com>
2023-12-23 19:45:20 -0500

    create object containing quesitons and options for wellness checkin cards

--- src/app/components/WellnessCheckIn.tsx
+++ src/app/components/WellnessCheckIn.tsx
@@ -1,7 +1,36 @@
 import React from "react";
 
 const WellnessCheckIn = () => {
-	return <div>WellnessCheckIn</div>;
+	interface checkInCard {
+		question: string;
+		option1text: string;
+		option2text: string;
+	}
+
+	const checkInCard: checkInCard[] = [
+		{
+			question: "did you drink enough water today?",
+			option1text: "yes",
+			option2text: "no",
+		},
+		{
+			question: "how stressed were you today?",
+			option1text: "just a little",
+			option2text: "very stressed",
+		},
+		{
+			question: "did you eat 3 meals today?",
+			option1text: "yes",
+			option2text: "no",
+		},
+		{
+			question: "did you have an uplifting conversation today?",
+			option1text: "yes",
+			option2text: "no",
+		},
+	];
+
+	return <React.Fragment></React.Fragment>;
 };
 
 export default WellnessCheckIn;
